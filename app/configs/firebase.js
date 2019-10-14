78e1ee9a559e86ccdf6db178d48f7b78715ec35e
Darosy Syaiful Imam <darosy.imam@gmail.com>
2019-10-15 04:56:16 +0700

    chat app

---
+++ app/configs/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAocSoMIPyvMZtQ9_Iw0tVWLoLNyDt_6YY",
+  authDomain: "chatapps-f61b2.firebaseapp.com",
+  databaseURL: "https://chatapps-f61b2.firebaseio.com",
+  projectId: "chatapps-f61b2",
+  storageBucket: "chatapps-f61b2.appspot.com",
+  messagingSenderId: "967122221539",
+  appId: "1:967122221539:web:5c5846c3c616ab4a17c8d6"
+};
